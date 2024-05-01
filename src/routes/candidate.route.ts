4cf6397b0f3d3c45962fd9ca166ae47048a795fb
shashwotbhattarai <shashwot.tech@gmail.com>
2024-05-02 05:07:18 +0545

    refactored the way instances were created

--- src/routes/candidate.route.ts
+++ src/routes/candidate.route.ts
@@ -4,18 +4,18 @@ import CandidateController from "../controllers/candidate.controller";
 
 const protectRoute = new AuthGuardMiddleware().protectRoute;
 
-const getAllCandidates = new CandidateController().getAllCandidates;
-
-const getOneCandidate = new CandidateController().getOneCandidate;
-
-const getCVDownloadUrl = new CandidateController().getDownloadURL;
+const candidateController = new CandidateController();
+const getAllCandidates =
+  candidateController.getAllCandidates.bind(candidateController);
+const getOneCandidate =
+  candidateController.getOneCandidate.bind(candidateController);
+const getCVDownloadUrl =
+  candidateController.getDownloadURL.bind(candidateController);
 
 const router: Router = express.Router();
 
 router.get("/all", protectRoute(["recruiter"]), getAllCandidates);
-
 router.get("/:user_id", protectRoute(["recruiter"]), getOneCandidate);
-
 router.get("/cv/:key", protectRoute(["recruiter"]), getCVDownloadUrl);
 
 export default router;
